7a66a4f5970d11ef2ca4f9c446afcb7524231b19
Zindre17 <roronoaaarstrand@gmail.com>
2017-03-06 11:48:48 +0100

    Create Tile.java

---
+++ core/src/com/mygdx/game/Tile.java
@@ -0,0 +1,42 @@
+package com.mygdx.game;
+
+/**
+ * Created by Zindre on 06-Mar-17.
+ */
+public class Tile {
+
+    private int x,y;
+    private int type;
+    private int tiles_to_portal;
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public Tile(int x, int y, int type) {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
+
