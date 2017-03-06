a6e0b3d2755b46818d2366d9fb402d1b6c92cac6
Masoom Maham <masoommaham@gmail.com>
2017-03-06 13:03:35 +0100

    Added a simple skeleton for a menu screen

--- core/src/com/mygdx/game/MenuView.java
+++ core/src/com/mygdx/game/MenuView.java
@@ -1,19 +1,39 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 /**
  * Created by Zindre on 06-Mar-17.
  */
 public class MenuView implements Screen {
+
+    SpriteBatch batch;
+    Texture img;
+    Game game;
+
+    public MenuView(Game game){
+        this.game = game;
+    }
+
     @Override
     public void show() {
-
+        batch = new SpriteBatch();
+        img = new Texture("badlogic.jpg");
+        System.out.println("testing print");
     }
 
     @Override
     public void render(float delta) {
-
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(img, 0, 0);
+        batch.end();
     }
 
     @Override
@@ -38,6 +58,7 @@ public class MenuView implements Screen {
 
     @Override
     public void dispose() {
-
+        batch.dispose();
+        img.dispose();
     }
 }
--- ios/src/com/mygdx/game/IOSLauncher.java
+++ ios/src/com/mygdx/game/IOSLauncher.java
@@ -5,13 +5,12 @@ import org.robovm.apple.uikit.UIApplication;
 
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
-import com.mygdx.game.towerDefence;
 
 public class IOSLauncher extends IOSApplication.Delegate {
     @Override
     protected IOSApplication createApplication() {
         IOSApplicationConfiguration config = new IOSApplicationConfiguration();
-        return new IOSApplication(new towerDefence(), config);
+        return new IOSApplication(new TowerDefence(), config);
     }
 
     public static void main(String[] argv) {
