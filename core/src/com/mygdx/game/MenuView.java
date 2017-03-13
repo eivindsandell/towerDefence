4eb5413a622d643014063bf147ff31e0b99fbcc6
Sindre <roronoaaarstrand@gmail.com>
2017-03-13 11:06:11 +0100

    lagt til buttonspack og buttonkode i menuview

--- core/src/com/mygdx/game/MenuView.java
+++ core/src/com/mygdx/game/MenuView.java
@@ -5,7 +5,12 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
 /**
  * Created by Zindre on 06-Mar-17.
@@ -20,6 +25,13 @@ public class MenuView implements Screen {
         this.game = game;
     }
 
+    Stage stage;
+    Skin skin;
+    TextureAtlas buttonAtlas;
+    BitmapFont font;
+    TextButton.TextButtonStyle textButtonStyle;
+    TextButton button;
+
     @Override
     public void show() {
         batch = new SpriteBatch();
@@ -31,10 +43,21 @@ public class MenuView implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        batch.draw(img, 0, 0);
-        batch.end();
+        Gdx.input.setInputProcessor(stage);
+        font = new BitmapFont();
+        skin = new Skin();
+        buttonAtlas = new TextureAtlas(Gdx.files.internal("buttons/buttons.pack"));
+        skin.addRegions(buttonAtlas);
+        textButtonStyle = new TextButton.TextButtonStyle();
+        textButtonStyle.font = font;
+        textButtonStyle.up = skin.getDrawable("blank-1");
+        textButtonStyle.down = skin.getDrawable("blank-2");
+        textButtonStyle.checked = skin.getDrawable("blank-1");
+        button = new TextButton("Button1", textButtonStyle);
+        stage.addActor(button);
     }
+
+
 
     @Override
     public void resize(int width, int height) {
