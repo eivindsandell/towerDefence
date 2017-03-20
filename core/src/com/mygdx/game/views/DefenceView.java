d1e9b9ad5f3e5d8f2fddea7598e0ffdb590a283a
Sindre <roronoaaarstrand@gmail.com>
2017-03-20 10:59:04 +0100

    tegner menybox i attack og defence-view

--- core/src/com/mygdx/game/views/DefenceView.java
+++ core/src/com/mygdx/game/views/DefenceView.java
@@ -4,10 +4,13 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.mygdx.game.Globals;
 
 public class DefenceView implements Screen {
     Game game;
-
+    Globals g = new Globals();
+    ShapeRenderer sr = new ShapeRenderer();
 
     public DefenceView(Game game){
         this.game = game;
@@ -22,6 +25,7 @@ public class DefenceView implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(201/255f, 163/255f, 14/255f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        displayUnitMenu();
     }
 
     @Override
@@ -46,6 +50,11 @@ public class DefenceView implements Screen {
 
     @Override
     public void dispose() {
-
+    }
+    private void displayUnitMenu(){
+        sr.begin(ShapeRenderer.ShapeType.Filled);
+        sr.setColor(0,0,0,1);
+        sr.rect(0,0,g.getScreenWith(),g.getScreenHeight()-g.getScreenWith());
+        sr.end();
     }
 }
